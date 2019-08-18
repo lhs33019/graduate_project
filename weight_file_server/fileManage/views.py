bfd9a3c0e35c24a8a97be35b7242903dfb8907aa
lim <lbs33019@gmail.com>
2019-08-18 19:22:53 +0900

    complete get list api

--- weight_file_server/fileManage/views.py
+++ weight_file_server/fileManage/views.py
@@ -10,13 +10,16 @@ from .forms import WeightFileForm
 # Create your views here.
 def getlist(request):
     if request.method == 'GET':
-        weightFiles = WeightFile.objects
+        weightFiles_list = list(WeightFile.objects.order_by('-created_at').values())
+
+        # weightFiles_list = serializers.serialize('json', weightFiles)
         # weightFiles = serializers.serialize('json', [weightFiles.all()[0], ])
+        # serialized_object = serializers.serialize('json', [WeightFile.objects.all(), ])
 
         return JsonResponse({
             'message' : 'Available Weight File List',
-            'Weight Files' : weightFiles,
-        }, json_dumps_params = {'ensure_ascii': False})
+            'Weight Files' : weightFiles_list,
+        }, json_dumps_params = {'ensure_ascii': True})
 
     else:
         return HttpResponseNotFound('Not valid request')
@@ -39,18 +42,24 @@ def send(request):
 
     
 def download_direct(request, path):
-    file_path = os.path.join(settings.MEDIA_ROOT, path)
-    if os.path.exists(file_path):
-        response = HttpResponse(open(file_path, 'rb'), content_type="application/force-download")
-        response['Content-Disposition'] = 'inline; filename=' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-        return response
-    return HttpResponseNotFound('There is no file')
+    if request.method == 'GET':
+        file_path = os.path.join(settings.MEDIA_ROOT, path)
+        if os.path.exists(file_path):
+            response = HttpResponse(open(file_path, 'rb'), content_type="application/force-download")
+            response['Content-Disposition'] = 'inline; filename=' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+            return response
+        return HttpResponseNotFound('There is no file')
+    else:
+        return HttpResponseNotFound('Not valid request')
 
 def download(request, file_id):
-    weightFiles = WeightFile.objects.get(pk=file_id)
-    file_path = os.path.join(settings.MEDIA_ROOT, weightFiles.weight_file.path)
-    if os.path.exists(file_path):
-        response = HttpResponse(open(file_path, 'rb'), content_type="application/force-download")
-        response['Content-Disposition'] = 'inline; filename=' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-        return response
-    return HttpResponseNotFound('There is no file')
+    if request.method == 'GET':
+        weightFiles = WeightFile.objects.get(pk=file_id)
+        file_path = os.path.join(settings.MEDIA_ROOT, weightFiles.weight_file.path)
+        if os.path.exists(file_path):
+            response = HttpResponse(open(file_path, 'rb'), content_type="application/force-download")
+            response['Content-Disposition'] = 'inline; filename=' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+            return response
+        return HttpResponseNotFound('There is no file')
+    else:
+        return HttpResponseNotFound('Not valid request')
