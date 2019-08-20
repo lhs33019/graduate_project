52209084069c7da53dd3a0f7caa76e5fc868876d
lim <lbs33019@gmail.com>
2019-08-20 11:13:27 +0900

    admin regist ImageFile model

--- weight_file_server/fileManage/admin.py
+++ weight_file_server/fileManage/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import WeightFile
+from .models import WeightFile, ImageFile
 
 # Register your models here.
 admin.site.register(WeightFile)
+admin.site.register(ImageFile)
